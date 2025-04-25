6a1163ca5706fb4d7cb7e817fbdbb14e81e92d8a
양영조 <a10019610@gmail.com>
2025-04-25 14:23:24 +0900

    chore: frontend 프로젝트 세팅

---
+++ frontend/src/layouts/Layout.tsx
@@ -0,0 +1,10 @@
+const Layout = () => {
+  return (
+    <div className="flex flex-col h-screen">
+      <header className="bg-gray-800 text-white p-4">Header</header>
+      <main className="flex-grow p-4">Main Content</main>
+      <footer className="bg-gray-800 text-white p-4">Footer</footer>
+    </div>
+  );
+};
+export default Layout;
---
+++ frontend/src/services/tmp.tsx
@@ -0,0 +1,9 @@
+const tmp = () => {
+  return (
+    <div>
+      <h1>Temporary Component</h1>
+      <p>This is a temporary component.</p>
+    </div>
+  );
+};
+export default tmp;
