311c035572fe40b3e1211d90eff4fa2ef38b26dc
양영조 <a10019610@gmail.com>
2025-04-28 12:27:09 +0900

    feat: 프로젝트 세팅 및 전역 디자인 시스템 설정

--- frontend/src/layouts/Layout.tsx
+++ frontend/src/layouts/Layout.tsx
@@ -1,10 +1,14 @@
+import { Outlet } from 'react-router-dom';
+
 const Layout = () => {
   return (
-    <div className="flex flex-col h-screen">
+    <div className="flex flex-col w-screen h-screen overflow-hidden">
       <header className="bg-gray-800 text-white p-4">Header</header>
-      <main className="flex-grow p-4">Main Content</main>
-      <footer className="bg-gray-800 text-white p-4">Footer</footer>
+      <main className="flex-grow overflow-hidden">
+        <Outlet />
+      </main>
     </div>
   );
 };
+
 export default Layout;
