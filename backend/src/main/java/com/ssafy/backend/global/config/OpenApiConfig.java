7affea2b863f4272b768ee7c668440afc78f7435
g1767g <g1767g@naver.com>
2025-04-29 20:58:18 +0900

    feat: [S12P31A604-48] Swagger Request URL 설정

--- backend/src/main/java/com/ssafy/backend/global/config/OpenApiConfig.java
+++ backend/src/main/java/com/ssafy/backend/global/config/OpenApiConfig.java
@@ -6,20 +6,32 @@ import org.springframework.context.annotation.Configuration;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.servers.Server;
+
+import java.util.List;
 
 @Configuration
 public class OpenApiConfig {
+
 	@Bean
 	public OpenAPI backendOpenAPI() {
 		return new OpenAPI()
-			.info(new Info()
-				.title("Liar Game : 믿지마! - API")
-				.version("v1.0.0")
-				.description("API 문서")
-				.license(new License()
-					.name("Apache 2.0")
-					.url("https://www.apache.org/licenses/LICENSE-2.0.html")
+				.info(new Info()
+						.title("Liar Game : 믿지마! - API")
+						.version("v1.0.0")
+						.description("API 문서")
+						.license(new License()
+								.name("Apache 2.0")
+								.url("https://www.apache.org/licenses/LICENSE-2.0.html")
+						)
 				)
-			);
+				.servers(List.of(
+						new Server()
+								.url("https://k12a604.p.ssafy.io")
+								.description("Prod"),
+						new Server()
+								.url("http://localhost:8080")
+								.description("Local")
+				));
 	}
 }
