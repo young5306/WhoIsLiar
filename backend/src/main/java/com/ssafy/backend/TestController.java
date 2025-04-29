3fc03f619154ed9d0414193c96eb14833b2971a0
g1767g <g1767g@naver.com>
2025-04-29 16:03:45 +0900

    cors

--- backend/src/main/java/com/ssafy/backend/TestController.java
+++ backend/src/main/java/com/ssafy/backend/TestController.java
@@ -5,7 +5,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api")
 public class TestController {
     @GetMapping("/test")
     public String test() {
