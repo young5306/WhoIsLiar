722b9f313cd28f54c4a4ae45eecb62a3acd93879
양영조 <a10019610@gmail.com>
2025-04-29 10:24:39 +0900

    fix: 배포를 위한 오류 수정 3

--- frontend/src/pages/main/RuleBookPage.tsx
+++ frontend/src/pages/main/RuleBookPage.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+// import { useNavigate } from 'react-router-dom';
 import { ChevronLeft, ChevronRight } from 'lucide-react'; // 아이콘 라이브러리 사용 (lucide-react 설치 필요)
 
 const ruleBookData = [
