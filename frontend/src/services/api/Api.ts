f715e0fbe0229d888eeed6c8c7c28932e8821451
양영조 <a10019610@gmail.com>
2025-04-29 10:19:27 +0900

    fix: api 인스턴스 수정

--- frontend/src/services/api/Api.ts
+++ frontend/src/services/api/Api.ts
@@ -13,20 +13,6 @@ const setupInterceptors = (instance: AxiosInstance) => {
     },
     (error) => Promise.reject(error)
   );
-
-  //   instance.interceptors.response.use(
-  //     (response) => response,
-  //     async (error) => {
-  //       if (error.response?.status === 401) {
-  //         // 401 인증 실패시 토큰 클리어
-  //         import('@/stores/useAuthStore').then(({ useAuthStore }) => {
-  //           useAuthStore.getState().clearAccessToken();
-  //         });
-  //         console.error('인증 실패: 로그아웃 처리됨');
-  //       }
-  //       return Promise.reject(error);
-  //     }
-  //   );
 };
 
 export const createAxiosInstance = (): AxiosInstance => {
@@ -37,8 +23,8 @@ export const createAxiosInstance = (): AxiosInstance => {
     withCredentials: true,
   });
 
-//   setupInterceptors(instance);
-//   return instance;
-// };
+  setupInterceptors(instance);
+  return instance;
+};
 
 export const api = createAxiosInstance();
