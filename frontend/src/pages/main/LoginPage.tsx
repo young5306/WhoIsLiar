0fe791bb0d9db2e6e2e618b75ac4ff8916380c89
최은영 <harrysunae@naver.com>
2025-04-29 14:56:08 +0900

    feat: 로그인 api 연결

--- frontend/src/pages/main/LoginPage.tsx
+++ frontend/src/pages/main/LoginPage.tsx
@@ -2,13 +2,31 @@ import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import GameButton from '../../components/common/GameButton';
 import InputField from '../../components/common/InputField';
+import { useAuthStore } from '../../stores/useAuthStore';
+import loginApi from '../../services/api/AuthService';
 
 const LoginPage = () => {
   const navigate = useNavigate();
   const [nickname, setNickname] = useState('');
+  const { setAccessToken, setUserInfo } = useAuthStore();
 
-  const handleLogin = () => {
-    navigate('/room-list');
+  const handleLogin = async () => {
+    try {
+      const { token, nickname: returnedNickname } = await loginApi(nickname);
+
+      setAccessToken(token);
+      setUserInfo({ token, nickname: returnedNickname });
+
+      navigate('/room-list');
+    } catch (err: any) {
+      if (err.response?.status === 409) {
+        alert('이미 사용 중인 닉네임입니다.');
+      } else if (err.response?.status === 400) {
+        alert('닉네임을 입력해주세요.');
+      } else {
+        alert('로그인 중 문제가 발생했습니다.');
+      }
+    }
   };
 
   return (
