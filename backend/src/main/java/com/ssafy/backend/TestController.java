86aae20411b9eb05c0d70dc9961bb7efae6a25f5
g1767g <g1767g@naver.com>
2025-04-29 13:47:42 +0900

    Test

--- backend/src/main/java/com/ssafy/backend/TestController.java
+++ backend/src/main/java/com/ssafy/backend/TestController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
-    @GetMapping("/test")
+    @GetMapping("/api/test")
     public String test() {
         return "ㅋㅋ";
     }
