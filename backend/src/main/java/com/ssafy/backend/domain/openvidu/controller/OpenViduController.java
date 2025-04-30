a55eb637e17a4575baa1d9337a8ceac7207b352e
g1767g <g1767g@naver.com>
2025-04-30 11:07:18 +0900

    feat: [S12P31A604-8] openvidu 설정 변경

--- backend/src/main/java/com/ssafy/backend/domain/openvidu/controller/OpenViduController.java
+++ backend/src/main/java/com/ssafy/backend/domain/openvidu/controller/OpenViduController.java
@@ -7,7 +7,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/openvidu")
+@RequestMapping("/openvidu")
 @RequiredArgsConstructor
 public class OpenViduController {
 
--- backend/src/main/java/com/ssafy/backend/domain/openvidu/service/OpenViduService.java
+++ backend/src/main/java/com/ssafy/backend/domain/openvidu/service/OpenViduService.java
@@ -13,14 +13,12 @@ public class OpenViduService {
 
 
     public String createSession(String customSessionId) throws OpenViduJavaClientException, OpenViduHttpException {
-        // 세션 설정
         SessionProperties properties = new SessionProperties.Builder()
-                .customSessionId(customSessionId) // optional, null 가능
-                .mediaMode(MediaMode.ROUTED)      // ROUTED(기본) 또는 RELAY
-                .recordingMode(RecordingMode.MANUAL) // 녹화 설정
+                .customSessionId(customSessionId)
+                .mediaMode(MediaMode.ROUTED)
+                .recordingMode(RecordingMode.MANUAL)
                 .build();
 
-        // 세션 생성
         Session session = openVidu.createSession(properties);
         return session.getSessionId();
     }
@@ -32,7 +30,6 @@ public class OpenViduService {
             throw new IllegalArgumentException("세션이 존재하지 않습니다: " + sessionId);
         }
 
-        // 연결 속성 설정
         ConnectionProperties connectionProperties = new ConnectionProperties.Builder()
                 .type(ConnectionType.WEBRTC)
                 .role(OpenViduRole.PUBLISHER)
