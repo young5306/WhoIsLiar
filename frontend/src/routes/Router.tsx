343e17aa29e1247ec04c974662d25220de0e282b
최은영 <harrysunae@naver.com>
2025-04-30 09:39:58 +0900

    feat: 방만들기 모달

--- frontend/src/routes/Router.tsx
+++ frontend/src/routes/Router.tsx
@@ -4,6 +4,7 @@ import MainPage from '../pages/main/MainPage';
 import RuleBookPage from '../pages/main/RuleBookPage';
 import LoginPage from '../pages/main/LoginPage';
 import WaitingRoomPage from '../pages/waitingRoom/WaitingRoomPage';
+import { RoomListPage } from '../pages/roomList/RoomListPage';
 
 const Router = () => {
   return (
@@ -11,6 +12,8 @@ const Router = () => {
       <Route element={<Layout />}>
         <Route path="/" element={<MainPage />} />
         <Route path="/login" element={<LoginPage />} />
+        <Route path="/room-list" element={<RoomListPage />} />
+        {/* <Route path="/game" element={<GamePage />} /> */}
         <Route path="/waitingRoom" element={<WaitingRoomPage />} />
       </Route>
       <Route path="/rule-book" element={<RuleBookPage />} />
