607a26809bd8437bb08dddc59b2666a0efdfa81e
g1767g <g1767g@naver.com>
2025-05-05 18:16:39 +0900

    feat: [S12P31A604-76] Redisson Config 추가

---
+++ backend/src/main/java/com/ssafy/backend/config/RedissonConfig.java
@@ -0,0 +1,27 @@
+package com.ssafy.backend.config;
+
+import org.redisson.Redisson;
+import org.redisson.api.RedissonClient;
+import org.redisson.config.Config;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RedissonConfig {
+
+	@Value("${redis.host}")
+	private String redisHost;
+
+	@Value("${redis.port}")
+	private String redisPort;
+
+	@Bean(destroyMethod = "shutdown")
+	public RedissonClient redissonClient() {
+		Config config = new Config();
+		config.useSingleServer()
+			.setAddress("redis://" + redisHost + ":" + redisPort);
+
+		return Redisson.create(config);
+	}
+}
