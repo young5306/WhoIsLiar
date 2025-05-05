ecc1b3ccb2c7f378c0d5d80bd3ffb8c825c9cacb
김상욱 <kimsangouk1020@gmail.com>
2025-05-05 22:34:10 +0900

    feat : websocket logger aop 및 브로커 단위 interceptor 추가

---
+++ backend/src/main/java/com/ssafy/backend/websocket/AuthTokenChannelInterceptor.java
@@ -0,0 +1,34 @@
+package com.ssafy.backend.websocket;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.MessageChannel;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
+import org.springframework.messaging.support.ChannelInterceptor;
+import org.springframework.messaging.support.MessageHeaderAccessor;
+import org.springframework.stereotype.Component;
+
+import com.ssafy.backend.domain.auth.service.AuthService;
+
+@Component
+public class AuthTokenChannelInterceptor implements ChannelInterceptor {
+
+	private final AuthService authService;
+
+	@Autowired
+	public AuthTokenChannelInterceptor(AuthService authService) {
+		this.authService = authService;
+	}
+
+	@Override
+	public Message<?> preSend(Message<?> message, MessageChannel channel) {
+		StompHeaderAccessor accessor = MessageHeaderAccessor.getAccessor(message, StompHeaderAccessor.class);
+		if (accessor != null && accessor.getSessionAttributes() != null) {
+			String token = (String) accessor.getSessionAttributes().get("token");
+			if (token != null) {
+				authService.validateAndRefresh(token);
+			}
+		}
+		return message;
+	}
+}
