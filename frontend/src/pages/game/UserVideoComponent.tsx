f72955b9a19c42038c595c038ae1202e54a7f267
yw <sapientia268@gmail.com>
2025-05-07 13:30:19 +0900

    feat: face-api.js & openvidu 연결

--- frontend/src/pages/game/UserVideoComponent.tsx
+++ frontend/src/pages/game/UserVideoComponent.tsx
@@ -9,25 +9,28 @@ interface UserVideoComponentProps {
 const UserVideoComponent: React.FC<UserVideoComponentProps> = ({
   streamManager,
 }) => {
-  // 사용자 닉네임 추출
-  const getNicknameTag = () => {
-    // OpenVidu의 connection.data에서 clientData(닉네임) 추출
-    try {
-      return JSON.parse(streamManager.stream.connection.data).clientData;
-    } catch {
-      return '';
-    }
-  };
+  // // 사용자 닉네임 추출
+  // const getNicknameTag = () => {
+  //   // OpenVidu의 connection.data에서 clientData(닉네임) 추출
+  //   try {
+  //     return JSON.parse(streamManager.stream.connection.data).clientData;
+  //   } catch {
+  //     return '';
+  //   }
+  // };
 
   return (
     <div>
       {streamManager !== undefined ? (
-        <div className="streamcomponent">
+        <div
+          className="streamcomponent w-full h-full "
+          style={{ transform: 'scaleX(-1) scale(1.2) translateY(-10px)' }}
+        >
           {/* 비디오 컴포넌트에 streamManager 전달 */}
           <OpenViduVideoComponent streamManager={streamManager} />
-          <div>
+          {/* <div>
             <p>{getNicknameTag()}</p>
-          </div>
+          </div> */}
         </div>
       ) : null}
     </div>
