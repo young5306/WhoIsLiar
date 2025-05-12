88635c6e6bcb8010663499fbb61a0696a07439b6
김상욱 <kimsangouk1020@gmail.com>
2025-05-12 12:59:50 +0900

    feat : 방 정렬 순서 변경

--- backend/src/main/java/com/ssafy/backend/domain/room/repository/RoomRepository.java
+++ backend/src/main/java/com/ssafy/backend/domain/room/repository/RoomRepository.java
@@ -5,6 +5,8 @@ import java.util.Optional;
 
 import com.ssafy.backend.domain.auth.entity.SessionEntity;
 import com.ssafy.backend.domain.room.entity.Room;
+
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -14,6 +16,8 @@ public interface RoomRepository extends JpaRepository<Room, Long> {
 	Optional<Room> findByRoomCode(String roomCode);
 	boolean existsBySession(SessionEntity session);
 	List<Room> findByRoomNameContaining(String roomName);
+
+	List<Room> findByRoomNameContaining(String roomName, Sort sort);
 
 	@Query("SELECT r FROM Room r JOIN FETCH r.session WHERE r.roomCode = :roomCode")
 	Optional<Room> findByRoomCodeFetchSession(@Param("roomCode") String roomCode);
